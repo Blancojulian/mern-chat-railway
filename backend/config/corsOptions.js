2c0b1b39ce96308530b48bbc150777283999f9f3
Blancojulian <julianblanco963@hotmail.com>
2024-02-12 02:43:30 -0300

    cambio

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -2,6 +2,11 @@ const whitelist = ['http://localhost:3000', 'http://localhost:6005']
 
 const corsOptions = {
   origin: function (origin, callback) {
+    if (origin) {
+      console.log('mismo origen');
+      console.log(origin);
+      
+    }
     //si es el mismo origen es undefined
     if (origin === undefined || whitelist.indexOf(origin) !== -1) {
       callback(null, true)
